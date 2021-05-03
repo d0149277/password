d944b59b3ad70fb07d0bb680583f6730b2136864
Ivy Wang <ivywty@gmail.com>
2021-05-04 02:10:05 +0800

    answer file

--- password/password.py
+++ password/password.py
@@ -1,15 +1,12 @@
 password = 'a123456'
-x = 0
+x = 3
 while True:
-    login = input('請輸入密碼: ')
-    x = x + 1
-    if login != password and x < 2:
-	    print('密碼錯誤!還有2次機會!')
-    elif login != password and x < 3:
-	    print('密碼錯誤!還有1次機會!')
-    elif login != password and x < 4:
-	    print('密碼錯誤!退出系統!')
-	    break
-    elif login == password:
-	    print('登入成功')
-	    break
+	login = input('請輸入密碼: ')
+	if login == password:
+		print('登入成功')
+		break
+	else:
+		x = x - 1
+		print('密碼錯誤!還有', x ,'次機會!')
+		if x == 0:
+			break
