c19f9dfcb6acb31027fb9b4ea22022959908d068
Ivy Wang <ivywty@gmail.com>
2021-05-04 02:28:52 +0800

    finished file

--- password/password.py
+++ password/password.py
@@ -1,12 +1,13 @@
 password = 'a123456'
 x = 3
-while True:
+while x > 0:
+	x = x - 1
 	login = input('請輸入密碼: ')
 	if login == password:
 		print('登入成功')
 		break
 	else:
-		x = x - 1
-		print('密碼錯誤!還有', x ,'次機會!')
-		if x == 0:
-			break
+		print('密碼錯誤!')
+		print('還有', x ,'次機會!')
+		if x == 0: 
+			print('密碼錯誤!帳號已凍結!')
